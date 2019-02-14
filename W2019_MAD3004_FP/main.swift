6f98d3b2638ab0daa5fe6b057b2173f5f9eb469c
Ashlyjayakrishnan <ashlybaby95@gmail.com>
2019-02-14 18:25:30 -0500

    Initial Commit

---
+++ W2019_MAD3004_FP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  W2019_MAD3004_FP
+//
+//  Created by Ashly Baby on 2019-02-14.
+//  Copyright © 2019 Ashly Baby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
