11e5acddceb1bffe36afa8b3258de90906f9320b
athirapaul <athirapaul24@gmail.com>
2019-02-14 19:04:53 -0500

    person added

---
+++ W2019_MAD3004_FP/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  W2019_MAD3004_FP
+//
+//  Created by Athira Paul on 2019-02-14.
+//  Copyright © 2019 Ashly Baby. All rights reserved.
+//
+
+import Foundation
+class Person{
+    
+}
